b5286123bd69c89024b7fcd4c1de8a8075d049b3
Mai Uchida <mai.uchida1119@gmail.com>
2021-11-13 17:39:44 +0900

    Initial Commit

---
+++ BMI calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI calculator
+//
+//  Created by Mai Uchida on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
